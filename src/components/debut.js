2e92170e2df54ac1441478931aaffae04f6e4fa3
Touradou2003 <cheikhtouradougueye@esp.sn>
2024-05-30 01:35:07 +0000

    caroussel

--- src/components/debut.js
+++ src/components/debut.js
@@ -13,21 +13,21 @@ export default function Debut(){
                 <img class="minilogo" src={vector} alt=""/>
                 <h1 class="titre">Valorisation</h1>
             </div>
-            <p>Valorisation de la production agricole locale grâce à des machines de transformation innovantes, améliorant l'efficacité et la qualité des produits.</p>
+            <p>Valorisation de la production agricole locale grâce à des machines de transformation innovantes...</p>
         </div>
         <div class="section">
             <div class="header">
                 <img class="minilogo" src={vector} alt=""/>
                 <h1 class="titre">Conception innovante</h1>
             </div>
-            <p>Développement et conception de machines de transformation agricole innovantes, répondant aux besoins spécifiques des producteurs locaux au Sénégal.</p>
+            <p>Développement et conception de machines de transformation agricole innovantes ...</p>
         </div>
         <div class="section">
             <div class="header">
                 <img class="minilogo" src={vector} alt=""/>
                 <h1 class="titre">Maintenance et Support</h1>
             </div>
-            <p>Service complet de maintenance et support technique pour garantir la performance et la durabilité des machines de transformation agricole.</p>
+            <p>Service complet de maintenance et support technique pour garantir la performance et...</p>
         </div>
     </div>
     </section>
--- src/components/decouvrir.js
+++ src/components/decouvrir.js
@@ -14,16 +14,16 @@ export default function Decouvrir()
           <div class="container">
         <button class="box">
             <img src={consulting} alt=""/>
-            <h1>Consulting <p>Expertise en développement de logiciels et applications, gestion de projets Big Data, sourcing industriel mondial, et ingénierie pour des solutions sur mesure et performantes.</p></h1>
+            <h1>Consulting</h1>
             
         </button>
         <button class="box">
             <img src={campus} alt=""/>
-            <h1 class="titreCampus">Campus<p>Programme d'acquisition d'expertise internationale pour jeunes ingénieurs diplômés, avec des missions de deux ans chez des industriels matures, revenant ensuite renforcer notre équipe consulting.</p></h1>
+            <h1 class="titreCampus">Campus</h1>
         </button>
         <button class="box">
             <img src={studio} alt=""/>
-            <h1>Studio<p>Accompagnement des startups innovantes dans le développement de produits et services, stratégie d'entreprise, et mise en réseau avec des experts pendant trois ans.</p></h1>
+            <h1>Studio</h1>
         </button>
     </div>
        </>
